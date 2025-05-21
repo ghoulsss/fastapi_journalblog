30691bd92ac16a9962026a0679c1360156c58b78
Pavlov Danil <danpavkzn@mail.ru>
2025-05-21 20:48:55 +0300

    cleaned

--- main.py
+++ main.py
@@ -1,56 +1,33 @@
-import asyncio
-
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from src.api_v1.crud.demo_auto_crud import demo_m2m
 from src.api_v1.routers import router as main_router
 
-# from src.db.database import drop_tables, create_tables, async_session_factory
-
-# from src.api_v1.crud.demo_auto_crud import demo_m2m
-# from src.core.config import static_files, templates
-# from src.db.database import async_session_factory
+from src.db.database import db_moves
 
 
-app = FastAPI(
-    title="Journal Blog",
-    contact={
-        "name": "Danil",
-        "url": "https://t.me/unrivaled_achilles",
-        "email": "danpavkzn@mail.ru",
-    },
-)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
 )
 
-# app.mount("static", static_files, name="static")
+
 app.include_router(main_router)
 
 
-@app.get("/")
-async def root(request: Request):
-    # return templates.TemplateResponse(
-    #     "home.html",
-    #     {
-    #         "request": request,
-    #     },
-    # )
+@app.get("")
+async def root():
     return {"info": "journalblog"}
 
 
-async def db_moves():
-    # await drop_tables()
-    # await create_tables()
-    # async with async_session_factory() as session:
-    #     await demo_m2m(session)
-    pass
+@app.get("/db-recreate")
+async def db_recreate():
+    stmt = await db_moves()
+    return {"status": "success"}
 
 
 if __name__ == "__main__":
-    # asyncio.run(db_moves())
     uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
--- src/api_v1/crud/demo_auto_crud.py
+++ src/api_v1/crud/demo_auto_crud.py
@@ -1,158 +1,16 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload, joinedload
+# from sqlalchemy import select
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy.orm import selectinload, joinedload
 
-from src.db.models import User, Article, Tag, Category, CheckAuthor
-
-
-async def create_user(
-    username: str,
-    email: str,
-    password: str,
-    session: AsyncSession,
-) -> User:
-    user = User(
-        username=username,
-        email=email,
-        password=password,
-    )
-
-    session.add(user)
-    await session.commit()
-    return user
+# from src.db.models import User, Article, Tag, Category, CheckAuthor
 
 
-async def create_article(
-    title: str,
-    content: str,
-    category_id: int,
-    session: AsyncSession,
-) -> Article:
-    article = Article(
-        title=title,
-        content=content,
-        category_id=category_id,
-    )
-
-    session.add(article)
-    await session.commit()
-    return article
-
-
-async def create_tag(
-    name: str,
-    session: AsyncSession,
-) -> Tag:
-    tag = Tag(
-        name=name,
-    )
-
-    session.add(tag)
-    await session.commit()
-    return tag
-
-
-async def create_category(
-    name: str,
-    session: AsyncSession,
-) -> Category:
-    category = Category(
-        name=name,
-    )
-
-    session.add(category)
-    await session.commit()
-    return category
-
-
-async def main_relations(
-    session: AsyncSession,
-):
-    user_1 = await create_user("john", "danpavkznm@mail.ru", "qwerty", session)
-    user_2 = await create_user("sam", "qaloto@mail.ru", "secret", session)
-
-    category_1 = await create_category("Дороги", session)
-    category_2 = await create_category("ПГС", session)
-
-    tag_1 = await create_tag("it в строительстве", session)
-    tag_2 = await create_tag("Сопромат", session)
-
-    article_1 = await create_article(
-        "Автодороги",
-        "road road road road",
-        category_id=category_1.id,
-        session=session,
-    )
-    article_2 = await create_article(
-        "Мосты",
-        "Мосты Мосты Мосты Мосты",
-        category_id=category_2.id,
-        session=session,
-    )
-
-    user_1 = await session.scalar(
-        select(User)
-        .where(User.id == user_1.id)
-        .options(
-            selectinload(User.article),
-        ),
-    )
-    user_2 = await session.scalar(
-        select(User)
-        .where(User.id == user_2.id)
-        .options(
-            selectinload(User.article),
-        ),
-    )
-
-    article_1 = await session.scalar(
-        select(Article)
-        .where(Article.id == article_1.id)
-        .options(
-            selectinload(Article.tag),
-            selectinload(Article.category),
-        ),
-    )
-    article_2 = await session.scalar(
-        select(Article)
-        .where(Article.id == article_2.id)
-        .options(
-            selectinload(Article.tag),
-            selectinload(Article.category),
-        ),
-    )
-
-    article_1.tag.append(tag_1)
-    article_1.tag.append(tag_2)
-    article_2.tag.append(tag_1)
-
-    # article_1.category_id = category_1.id
-    # article_2.category_id = category_2.id
-
-    user_1.article.append(article_1)
-    user_2.article.append(article_1)
-    user_2.article.append(article_2)
-
-    await session.commit()
-
-
-async def get_users_with_articles(session: AsyncSession) -> list[User]:
-    stmt = (
-        select(User)
-        .options(
-            selectinload(User.article),
-        )
-        .order_by(User.id)
-    )
-    data = await session.scalars(stmt)
-    return list(data)
-
-
-# async def get_articles_full(session: AsyncSession) -> list[Article]:
+# async def get_articles_full_with_users(session: AsyncSession) -> list[Article]:
 #     stmt = (
 #         select(Article)
 #         .options(
 #             selectinload(Article.tag),
+#             selectinload(Article.user),
 #             joinedload(Article.category),
 #         )
 #         .order_by(Article.id)
@@ -161,38 +19,9 @@ async def get_users_with_articles(session: AsyncSession) -> list[User]:
 #     return list(data)
 
 
-async def get_articles_full_with_users(session: AsyncSession) -> list[Article]:
-    stmt = (
-        select(Article)
-        .options(
-            selectinload(Article.tag),
-            selectinload(Article.user),
-            joinedload(Article.category),
-        )
-        .order_by(Article.id)
-    )
-    data = await session.scalars(stmt)
-    return list(data)
-    # articles = await session.execute(
-    #     (
-    #         select(Article)
-    #         .options(
-    #             selectinload(Article.tag),
-    #             selectinload(Article.user),
-    #             joinedload(Article.category),
-    #         )
-    #         .offset(offset)
-    #         .limit(limit)
-    #         .order_by(Article.id)
-    #     )
-    # )
-    # return articles.scalars().all()
-
-
 # async def demo_m2m(
 #     session: AsyncSession,
 # ):
-#     # await main_relations(session)
 #     users = await get_users_with_articles(session)
 #     for i, user in enumerate(users):
 #         print(
