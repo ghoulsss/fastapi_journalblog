7622c8c05d940e5a0579fad73e24f2284e4a0edc
Pavlov Danil <danpavkzn@mail.ru>
2025-05-21 20:43:38 +0300

    bd for postgres remaked

--- src/db/database.py
+++ src/db/database.py
@@ -2,7 +2,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 
 from config import settings
-from config import BASE_DIR
 
 
 class Base(DeclarativeBase):
@@ -15,18 +14,16 @@ class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
 
 
-
-async_engine = create_async_engine(url=settings.DATABASE_URL_asyncpg, echo=True) # POSTGRESQL
-async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False) # POSTGRESQL
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg, echo=True
+)  # POSTGRESQL
+async_session_factory = async_sessionmaker(
+    async_engine, expire_on_commit=False
+)  # POSTGRESQL
 
 # DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/test.db"  # для sqlite
-# async_engine = create_async_engine(url=DATABASE_URL, echo=True)  # sqlite
-# async_session_factory = async_sessionmaker(
-#     async_engine, expire_on_commit=False
-# )  # sqlite
 
 
-# потом удалить
 async def create_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -40,3 +37,8 @@ async def drop_tables():
 async def get_async_session():
     async with async_session_factory() as session:
         yield session
+
+
+async def db_moves():
+    await drop_tables()
+    await create_tables()
