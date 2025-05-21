ae23451ee7cbd9f87bdb49c89b95241e1df3f0be
Pavlov Danil <danpavkzn@mail.ru>
2025-05-21 20:44:17 +0300

    postgres worked \ermaked env problems

--- config.py
+++ config.py
@@ -11,24 +11,21 @@ class AuthJWT(BaseModel):
     algorithm: str = "RS256"
     access_token_expired_minutes: int = 15
     refresh_token_expired_days: int = 30
-    
-    
+
+
 class Settings(BaseSettings):
-    postgres_db_host: str
-    postgres_db_port: int
-    postgres_db_user: str
-    postgres_db_password: str
-    postgres_db_name: str
+    postgres_host: str
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.postgres_db_user}:{self.postgres_db_password}@{self.postgres_db_host}:{self.postgres_db_port}/{self.postgres_db_name}"
-    
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
     auth_jwt: AuthJWT = AuthJWT()
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_prefix="POSTGRES_DB_",
-    )
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
